8ca7e80643a1a575e7a3ad17070abb838d5c8b2d
Dimitar Zlatkov <dimitar.zlatkov@musala.com>
2015-11-07 12:38:51 +0200

    Initial commit

---
+++ EuroCoinCollector/gen/com/dimitar/zlatkov/eurocoincollector/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.dimitar.zlatkov.eurocoincollector;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
